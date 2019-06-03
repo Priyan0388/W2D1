c1bbfd759350ed6c7a3ccd0552291b4932ed1f42
Priyan0388 <priyanthan.s@hotmail.com>
2019-06-03 17:11:56 +0000

    solved Printing in Chuncks

---
+++ getPrintHTML.js
@@ -0,0 +1,20 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks (data) {
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+    };
+    https.get(requestOptions, function (response) {
+        response.setEncoding('utf8');
+        response.on('data', function (data) {
+          console.log('Chunk Received. Length:\n', data);
+        });
+
+        response.on('end', function (){
+            console.log('Response stream complete')
+        })
+    })
+}
+
+console.log(getAndPrintHTMLChunks())
